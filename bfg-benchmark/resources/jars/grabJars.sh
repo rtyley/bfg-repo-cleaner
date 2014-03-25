0f95ce03becb05d1d354bff4b0367086d849570b
Roberto Tyley <roberto.tyley@guardian.co.uk>
2014-03-25 15:56:41 +0000

    Grab newer artifact releases...

--- bfg-benchmark/resources/jars/grabJars.sh
+++ bfg-benchmark/resources/jars/grabJars.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-for i in 4 5 6 7 9 10 11
+for i in 4.0 5.0 6.0 7.0 9.0 10.0 11.0 11.1 11.2
 do
-	VERSION="1.$i.0"
+	VERSION="1.$i"
 	curl -O "http://repo1.maven.org/maven2/com/madgag/bfg/$VERSION/bfg-$VERSION.jar"
 done
