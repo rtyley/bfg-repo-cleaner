204889aa473a48996c358032ee0a5c3d74bac373
Roberto Tyley <roberto.tyley@gmail.com>
2014-06-09 21:12:05 +0100

    Bump dependency versions

--- project/dependencies.scala
+++ project/dependencies.scala
@@ -2,13 +2,13 @@ import sbt._
 
 object Dependencies {
 
-  val scalaGitVersion = "2.3"
+  val scalaGitVersion = "2.4"
 
   val scalaGit = "com.madgag.scala-git" %% "scala-git" % scalaGitVersion
 
   val scalaGitTest = "com.madgag.scala-git" %% "scala-git-test" % scalaGitVersion
 
-  val specs2 = "org.specs2" %% "specs2" % "2.3.11"
+  val specs2 = "org.specs2" %% "specs2" % "2.3.12"
 
   val madgagCompress = "com.madgag" % "util-compress" % "1.33"
 
@@ -16,7 +16,7 @@ object Dependencies {
 
   val scopt = "com.github.scopt" %% "scopt" % "3.2.0"
 
-  val guava = Seq("com.google.guava" % "guava" % "16.0.1", "com.google.code.findbugs" % "jsr305" % "2.0.1")
+  val guava = Seq("com.google.guava" % "guava" % "17.0", "com.google.code.findbugs" % "jsr305" % "2.0.1")
 
   val scalaIoFile = "com.madgag" %% "scala-io-file" % "0.4.2"
 
