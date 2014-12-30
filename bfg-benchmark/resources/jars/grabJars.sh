869c319d2665819c5e4cfe663f551610894d4c8d
Roberto Tyley <roberto.tyley@guardian.co.uk>
2014-12-30 22:22:12 +0000

    Get latest jars

--- bfg-benchmark/resources/jars/grabJars.sh
+++ bfg-benchmark/resources/jars/grabJars.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-for i in 4.0 5.0 6.0 7.0 9.0 10.0 11.0 11.1 11.2 11.5 11.6 11.7
+for i in 4.0 5.0 6.0 7.0 9.0 10.0 11.0 11.1 11.2 11.5 11.6 11.7 11.10 12.0
 do
 	VERSION="1.$i"
 	curl -O "http://repo1.maven.org/maven2/com/madgag/bfg/$VERSION/bfg-$VERSION.jar"
