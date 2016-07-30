8b2b2d46500df079b584974a8d2c742466a81c34
D German <dmg@uvic.ca>
2016-07-29 18:28:08 -0700

    --blob-exec: Fixed the spawning of the subprocess.
    It was not properly waiting for the child processes, making it to hang
    under some architectures.
    
    At this point I am hardcoding a lot of the processing (e.g. process only
    .[ch]^ files

--- bfg-library/src/main/scala/com/madgag/git/bfg/cleaner/BlobExecModifier.scala
+++ bfg-library/src/main/scala/com/madgag/git/bfg/cleaner/BlobExecModifier.scala
@@ -6,6 +6,13 @@ import com.madgag.git.ThreadLocalObjectDatabaseResources
 import java.util.{Arrays => JavaArrays}
 import org.eclipse.jgit.lib.Constants.OBJ_BLOB
 import scala.collection.JavaConversions._
+import scalax.io._
+import scalax.io.managed._
+import java.io._
+import scala.sys.process._
+import scala.sys.process.ProcessIO
+import scala.io.Source
+import scala.collection.mutable.ArrayBuffer
 
 trait BlobExecModifier extends TreeBlobModifier {
 
@@ -13,35 +20,69 @@ trait BlobExecModifier extends TreeBlobModifier {
 
   val threadLocalObjectDBResources: ThreadLocalObjectDatabaseResources
 
-  def fix(entry: TreeBlobEntry) = {
+  def execute(entry: TreeBlobEntry) = {
+
     val loader = threadLocalObjectDBResources.reader().open(entry.objectId)
     val objectStream = loader.openStream
-    val variables = System.getenv().map { case (key, value) => s"$key=$value" }.toSeq :+
-      s"BFG_BLOB=${entry.objectId.name}"
-    val process = Runtime.getRuntime.exec(command, variables.toArray)
+    val fileName = entry.filename.toString
+
 
     val bytes = ByteStreams.toByteArray(objectStream)
-    objectStream.close()
-    process.getOutputStream.write(bytes)
-    process.getOutputStream.close()
 
-    ByteStreams.copy(process.getErrorStream, System.err)
-    process.getErrorStream.close()
+//    println(s"${entry.objectId.name}: 00 To execute : ${command} Length: ${bytes.length} on file ${entry.filename}")
 
-    val newBytes = ByteStreams.toByteArray(process.getInputStream)
-    process.getInputStream.close()
+    val newBytes = ArrayBuffer[Byte]()
 
-    val exitCode = process.waitFor()
-    if(exitCode != 0) {
-      throw new RuntimeException(s"Process exited with code $exitCode")
+    def readJob(in: InputStream) {
+
+      newBytes.appendAll(Stream.continually(in.read).takeWhile(_ != -1).map(_.toByte).toArray)
+      in.close();
+
     }
 
-    if(JavaArrays.equals(bytes, newBytes)) {
+    def writeJob(out: OutputStream) {
+      out.write(bytes)
+      out.close()
+    }
+
+    val io = new ProcessIO(
+      writeJob,
+      readJob,
+      _=> (),
+      false
+    )
+
+    val pb = Process(command, None, "BFG_BLOB" -> entry.objectId.name, "BFG_FILENAME" -> fileName)
+    val proc = pb.run(io)
+    val exitCode = proc.exitValue
+
+//    println(s"${entry.objectId.name}: 20 {$fileName} Waiting for BFG_BLOB Length: ${bytes.length} New bytes: ${newBytes.length} ExitCode ${exitCode}")
+
+    //    if(JavaArrays.equals(bytes, newBytes)) {
+
+
+    if (exitCode != 0 || JavaArrays.equals(bytes, newBytes.toArray)) {
+//      println(s"${entry.objectId.name}: 31 Nothing to do {$fileName}" )
       entry.withoutName
     } else {
-      val objectId = threadLocalObjectDBResources.inserter().insert(OBJ_BLOB, newBytes)
+//      println(s"${entry.objectId.name}: 35 to replace {$fileName}" )
+
+      val objectId = threadLocalObjectDBResources.inserter().insert(OBJ_BLOB, newBytes.toArray)
       entry.copy(objectId = objectId).withoutName
     }
   }
 
+
+  def fix(entry: TreeBlobEntry) = {
+
+    val fileName = entry.filename.toString
+    //    val toProcess = "^net(.+)\\.[ch]$".r
+    val toProcess = "(.+)\\.[ch]$".r
+
+    fileName match {
+      case toProcess(_) => execute(entry)
+      case _ => entry.withoutName
+
+    }
+  }
 }
