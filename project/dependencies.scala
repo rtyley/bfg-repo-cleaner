db58dfc784dffa8a6931c83584874829d907d841
Roberto Tyley <roberto.tyley@gmail.com>
2016-08-21 13:25:37 +0100

    Update to JGit v4.4.1

--- project/dependencies.scala
+++ project/dependencies.scala
@@ -6,7 +6,7 @@ object Dependencies {
 
   val jgitVersionOverride = Option(System.getProperty("jgit.version"))
 
-  val jgitVersion = jgitVersionOverride.getOrElse("4.4.0.201606070830-r")
+  val jgitVersion = jgitVersionOverride.getOrElse("4.4.1.201607150455-r")
 
   val jgit = "org.eclipse.jgit" % "org.eclipse.jgit" % jgitVersion
 
