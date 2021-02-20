b696168c14c087b271eaf04b01a787d62d71bb04
Roberto Tyley <roberto.tyley@guardian.co.uk>
2021-02-20 22:10:51 +0000

    Grab a smaller selection of jars for benchmarking
    slightly quicker download!

--- bfg-benchmark/resources/jars/grabJars.sh
+++ bfg-benchmark/resources/jars/grabJars.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-for i in 4.0 5.0 6.0 7.0 9.0 10.0 11.0 11.1 11.2 11.5 11.6 11.7 11.10 12.0
+for i in 4.0 5.0 6.0 7.0 12.0 13.0 13.1 13.2
 do
 	VERSION="1.$i"
-	curl -O "http://repo1.maven.org/maven2/com/madgag/bfg/$VERSION/bfg-$VERSION.jar"
+	curl -O "https://repo1.maven.org/maven2/com/madgag/bfg/$VERSION/bfg-$VERSION.jar"
 done
